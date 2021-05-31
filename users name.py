6f3bc81c21d89b3654682eda137a98809bde5b1d
luca-72 <luca.andres72@gmail.com>
2021-05-31 08:34:09 -0700

    new lesson user name

---
+++ users name.py
@@ -0,0 +1,5 @@
+### Write a program which accepts the users first and last name and prints them in reverse order with a space between them.
+
+fname = input("Input your first name : ")
+lname = input("Input your Last name : ")
+print("nice to meet you " + lname + " " + fname)
